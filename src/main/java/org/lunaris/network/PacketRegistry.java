b8a3484b7740963ae4fa41d357697ff2abd24653
RinesThaix <iam@kostya.sexy>
2018-07-20 01:52:17 +0300

    Регистрация пакетов

--- src/main/java/org/lunaris/network/PacketRegistry.java
+++ src/main/java/org/lunaris/network/PacketRegistry.java
@@ -1,7 +1,6 @@
 package org.lunaris.network;
 
-import org.lunaris.network.packet.Packet01Login;
-import org.lunaris.network.packet.Packet02PlayStatus;
+import org.lunaris.network.packet.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,7 +16,88 @@ public class PacketRegistry {
     public PacketRegistry() {
         registerPackets(
                 Packet01Login::new,
-                Packet02PlayStatus::new
+                Packet02PlayStatus::new,
+                Packet03EncryptionRequest::new,
+                Packet04EncryptionResponse::new,
+                Packet05Disconnect::new,
+                Packet06ResourcePacksInfo::new,
+                Packet07ResourcePackStack::new,
+                Packet08ResourcePackResponse::new,
+                Packet09Text::new,
+                Packet0AWorldTime::new,
+                Packet0BStartGame::new,
+                Packet0CAddPlayer::new,
+                Packet0DAddEntity::new,
+                Packet0ERemoveEntity::new,
+                Packet0FAddItem::new,
+                //Packet10
+                Packet11PickupItem::new,
+                Packet12MoveEntity::new,
+                Packet13MovePlayer::new,
+                //Packet14
+                Packet15UpdateBlock::new,
+                //Packet16
+                //Packet17
+                Packet18LevelSoundEvent::new,
+                Packet19LevelEvent::new,
+                //Packet1A
+                Packet1BEntityEvent::new,
+                //Packet1C
+                Packet1DUpdateAttributes::new,
+                Packet1EInventoryTransaction::new,
+                Packet1FMobEquipment::new,
+                Packet20MobArmorEquipment::new,
+                //Packet21
+                //Packet22
+                //Packet23
+                Packet24PlayerAction::new,
+                //Packet25
+                //Packet26
+                Packet27SetEntityData::new,
+                Packet28SetEntityMotion::new,
+                //Packet29
+                //Packet2A
+                Packet2BSetSpawnPosition::new,
+                Packet2CAnimate::new,
+                Packet2DRespawn::new,
+                Packet2EContainerOpen::new,
+                Packet2FContainerClose::new,
+                Packet30PlayerHotbar::new,
+                Packet31InventoryContent::new,
+                Packet32InventorySlot::new,
+                //Packet33
+                //Packet34
+                //Packet35
+                //Packet36
+                Packet37AdventureSettings::new,
+                //Packet38
+                //Packet39
+                Packet3AFullChunkData::new,
+                Packet3BSetCommandsEnabled::new,
+                //Packet3C
+                //Packet3D
+                Packet3ESetPlayerGameType::new,
+                Packet3FPlayerList::new,
+                //Packet40
+                //Packet41
+                //Packet42
+                //Packet43
+                //Packet44
+                Packet45RequestChunkRadius::new,
+                Packet46ChunkRadiusUpdate::new,
+                //Packet47
+                //Packet48
+                //Packet49
+                //Packet4A
+                //Packet4B
+                Packet4CAvailableCommands::new,
+                Packet4DCommandRequest::new,
+                //Packet4E
+                //Packet4F
+                //Packet50
+                //Packet51
+                Packet52ResourcePackDataInfo::new
+                //???
         );
     }
 

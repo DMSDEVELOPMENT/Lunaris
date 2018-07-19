02057abb4e1fa50805e9ca6e9222ec6c22e7a7e8
RinesThaix <iam@kostya.sexy>
2018-07-20 00:22:24 +0300

    Опечатка в джавадоке апи

--- src/main/java/org/lunaris/api/util/Internal.java
+++ src/main/java/org/lunaris/api/util/Internal.java
@@ -4,7 +4,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * It's very likely you don't have to use methods/constructors with this annotation yourself.
+ * It's very likely you have to use methods/constructors with this annotation yourself.
  * They are designed for internal usage only.
  */
 @Retention(RetentionPolicy.SOURCE)
